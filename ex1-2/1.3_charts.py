868a89d45f804e8d0d00adb156aea055cf8881de
Anna Harvey <anharvey@my365.bellevue.edu>
2021-01-10 19:50:23 -0500

    Initial upload week 4

---
+++ ex1-2/1.3_charts.py
@@ -0,0 +1,13 @@
+# Anna Harvey
+# DSC 640
+# 12/13/20
+# Assignment 1.3 - Charts
+#
+#
+import os
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+hotdog_places = pd.read_excel('hotdog-places.xlsm')
